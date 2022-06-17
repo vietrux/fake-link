e0353a3f535f319b14741817c67308cd5b630764
unknown <leviettrung.social@gmail.com>
2022-06-17 19:52:32 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,11 @@
+var link = document.getElementById('link');
+//change the href attribute to www.google.com when hover
+link.onmouseover = () => link.setAttribute("href", "https://www.google.com");
+//change the href attribute to www.google.com when not hover
+link.onmouseout = () => link.setAttribute("href", "https://www.google.com"); 
+//change the href attribute to ex.html when right click
+link.oncontextmenu = () => link.setAttribute("href", "./fake-link/ex.html");
+//change the href attribute to ex.html when left click
+link.onclick = () => link.setAttribute("href", "./fake-link/ex.html");
+//change the href attribute to ex.html when middle click
+link.onmousedown = () => link.setAttribute("href", "./fake-link/ex.html");
